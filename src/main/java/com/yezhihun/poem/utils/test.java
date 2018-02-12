cf5fdcd4a7a80bf6c5d7f22f279361033ff935d5
tianye <yezhihun@163.com>
2018-02-12 14:49:05 +0800

    update

---
+++ src/main/java/com/yezhihun/poem/utils/test.java
@@ -0,0 +1,20 @@
+package com.yezhihun.poem.utils;
+
+/**
+ * Created by tianye on 2017/9/17.
+ */
+public class test {
+
+
+    public static void main (String[] args){
+        System.out.println("test");
+
+        System.out.println("es");
+
+        test1();
+    }
+
+    public static void test1(){
+        System.out.println("t");
+    }
+}
